3ed3327969f5a0ca4e731c652c761ca4261ea8c2
Simeon Manolov <s.manolloff@gmail.com>
2024-09-16 15:06:54 +0300

    std::filesystem is not supported on mobile, use boost

--- common.h
+++ common.h
@@ -24,7 +24,7 @@ namespace MMAI {
     // Enum-to-int need C++23 to use std::to_underlying
     // https://en.cppreference.com/w/cpp/utility/to_underlying
     #define EI(enum_value) static_cast<int>(enum_value)
-    #define ASSERT(cond, msg) if(!(cond)) throw std::runtime_error(std::string("Assertion failed in ") + std::filesystem::path(__FILE__).filename().string() + ": " + msg)
+    #define ASSERT(cond, msg) if(!(cond)) throw std::runtime_error(std::string("Assertion failed in ") + boost::filesystem::path(__FILE__).filename().string() + ": " + msg)
 
     #define BF_XMAX 15    // GameConstants::BFIELD_WIDTH - 2 (ignore "side" cols)
     #define BF_YMAX 11    // GameConstants::BFIELD_HEIGHT
