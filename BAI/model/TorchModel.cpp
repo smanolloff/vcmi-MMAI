b88a0c5982fe04a890ac75969e44cfdc187f4db8
Simeon Manolov <s.manolloff@gmail.com>
2025-09-27 10:59:28 +0300

    try hack for MSVC LNK2005

--- BAI/model/TorchModel.cpp
+++ BAI/model/TorchModel.cpp
@@ -8,6 +8,10 @@
 
 #include "TorchModel.h"
 
+// Hack preventing MSVC LNK2005 error for multiply defined std::vector::~vector symbols
+// My guess is that CAddInfo causes this by publicly inheriting std::vector
+#include "lib/bonuses/Bonus.h"
+
 namespace MMAI::BAI {
 
 using TensorPtr = executorch::extension::TensorPtr;
